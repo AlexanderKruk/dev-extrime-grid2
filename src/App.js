cff7c26a39152c3d1a72d0d73267145284e4cce0
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-05-12 09:04:14 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
-import React from 'react';
-import './App.css';
-import comments from './comments';
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import {
+  TreeDataState,
+  CustomTreeData,
+} from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableTreeColumn,
+} from '@devexpress/dx-react-grid-material-ui';
 
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui';
+import {
+  generateRows,
+  defaultColumnValues,
+} from './demo-data/generator';
 
-const App = () => (
-  <Grid
-    rows={ comments }
-    columns={[
-      { name: 'name', title: 'Name' },
-      { name: 'email', title: 'Email' },
-      { name: 'body', title: 'Body' },
-    ]}>
-    <Table />
-    <TableHeaderRow />
-  </Grid>
-);
+const getChildRows = (row, rootRows) => {
+  const childRows = rootRows.filter(r => r.parentId === (row ? row.id : null));
+  return childRows.length ? childRows : null;
+};
 
-export default App;
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columns: [
+        { name: 'name', title: 'Name' },
+        { name: 'sex', title: 'Sex' },
+        { name: 'city', title: 'City' },
+        { name: 'car', title: 'Car' },
+      ],
+      tableColumnExtensions: [
+        { columnName: 'name', width: 300 },
+      ],
+      defaultExpandedRowIds: [0, 1],
+      data: generateRows({
+        columnValues: {
+          id: ({ index }) => index,
+          parentId: ({ index, random }) => (index > 0 ? Math.trunc((random() * index) / 2) : null),
+          ...defaultColumnValues,
+        },
+        length: 20,
+      }),
+    };
+  }
+
+  render() {
+    const {
+      data, columns, tableColumnExtensions, defaultExpandedRowIds,
+    } = this.state;
+
+    return (
+      <Paper>
+        <Grid
+          rows={data}
+          columns={columns}
+        >
+          <TreeDataState
+            defaultExpandedRowIds={defaultExpandedRowIds}
+          />
+          <CustomTreeData
+            getChildRows={getChildRows}
+          />
+          <Table
+            columnExtensions={tableColumnExtensions}
+          />
+          <TableHeaderRow />
+          <TableTreeColumn
+            for="name"
+          />
+        </Grid>
+      </Paper>
+    );
+  }
+}
