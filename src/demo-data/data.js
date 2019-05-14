67e647d51d0bbc573e13e9e298f989d3e847c51b
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-05-14 11:28:26 +0300

    first three with additional data

--- src/demo-data/data.js
+++ src/demo-data/data.js
@@ -359,4 +359,454 @@ export default [
         "volume_%": "+15%",
         "price": 0.02690229
       },
+      {
+          "id": 102,
+          "parentId": 28,
+          "coin": "06.05-12.05",
+          "period": "1w",
+          "volume": 45355.33 ,
+          "volume_%": "+12%",
+          "price": 0.02690529
+        },
+        {
+          "id": 103,
+          "parentId": 102,
+          "coin": "06.05",
+          "period": "1d",
+          "volume": 6323.33 ,
+          "volume_%": "+7%",
+          "price": 0.02690529
+        },
+        {
+          "id": 104,
+          "parentId": 102,
+          "coin": "07.05",
+          "period": "1d",
+          "volume": 5899.33 ,
+          "volume_%": "+3%",
+          "price": 0.02690529
+        },
+        {
+          "id": 105,
+          "parentId": 102,
+          "coin": "08.05",
+          "period": "1d",
+          "volume": 4642.33 ,
+          "volume_%": "+10%",
+          "price": 0.02690529
+        },
+        {
+          "id": 106,
+          "parentId": 102,
+          "coin": "09.05",
+          "period": "1d",
+          "volume": 5700.33 ,
+          "volume_%": "-5%",
+          "price": 0.02690529
+        },
+      {
+        "id": 107,
+        "parentId": 102,
+        "coin": "10.05",
+        "period": "1d",
+        "volume": 8532.33 ,
+        "volume_%": "-15%",
+        "price": 0.02690529
+      },
+      {
+          "id": 108,
+          "parentId": 102,
+          "coin": "11.05",
+          "period": "1d",
+          "volume": 5321.33 ,
+          "volume_%": "+25%",
+          "price": 0.02690529
+        },
+        {
+          "id": 109,
+          "parentId": 102,
+          "coin": "12.05",
+          "period": "1d",
+          "volume": 3542.33 ,
+          "volume_%": "+13%",
+          "price": 0.02690529
+        },
+        {
+          "id": 110,
+          "parentId": 103,
+          "coin": "00:00-04:00",
+          "period": "4h",
+          "volume": 1580.33 ,
+          "volume_%": "+4%",
+          "price": 0.02690529
+        },
+        {
+          "id": 111,
+          "parentId": 103,
+          "coin": "04:00-08:00",
+          "period": "4h",
+          "volume": 1202.33 ,
+          "volume_%": "+10%",
+          "price": 0.02690529
+        },
+        {
+          "id": 112,
+          "parentId": 103,
+          "coin": "08:00-12:00",
+          "period": "4h",
+          "volume": 1502.33 ,
+          "volume_%": "-13%",
+          "price": 0.02690529
+        },
+        {
+          "id": 113,
+          "parentId": 103,
+          "coin": "12:00-16:00",
+          "period": "4h",
+          "volume": 1802.33 ,
+          "volume_%": "+6%",
+          "price": 0.02690529
+        },
+        {
+          "id": 114,
+          "parentId": 103,
+          "coin": "16:00-20:00",
+          "period": "4h",
+          "volume": 1982.33 ,
+          "volume_%": "+2%",
+          "price": 0.02690529
+        },
+        {
+          "id": 115,
+          "parentId": 103,
+          "coin": "20:00-24:00",
+          "period": "4h",
+          "volume": 1023.33 ,
+          "volume_%": "-21%",
+          "price": 0.02690529
+        },
+        {
+          "id": 116,
+          "parentId": 110,
+          "coin": "00:00-01:00",
+          "period": "1h",
+          "volume": 250.33 ,
+          "volume_%": "+3%",
+          "price": 0.02690529
+        },
+        {
+          "id": 117,
+          "parentId": 110,
+          "coin": "01:00-02:00",
+          "period": "1h",
+          "volume": 300.33 ,
+          "volume_%": "+15%",
+          "price": 0.02690529
+        },
+        {
+          "id": 118,
+          "parentId": 110,
+          "coin": "02:00-03:00",
+          "period": "1h",
+          "volume": 182.33 ,
+          "volume_%": "-50%",
+          "price": 0.02690529
+        },
+        {
+          "id": 119,
+          "parentId": 110,
+          "coin": "03:00-04:00",
+          "period": "1h",
+          "volume": 204.33 ,
+          "volume_%": "+12%",
+          "price": 0.02690529
+        },
+        {
+          "id": 120,
+          "parentId": 116,
+          "coin": "00:00-00:15",
+          "period": "15m",
+          "volume": 55.33 ,
+          "volume_%": "+8%",
+          "price": 0.02690529
+        },
+        {
+          "id": 122,
+          "parentId": 116,
+          "coin": "00:15-00:30",
+          "period": "15m",
+          "volume": 63.33 ,
+          "volume_%": "+10%",
+          "price": 0.02690529
+        },   
+        {
+          "id": 123,
+          "parentId": 116,
+          "coin": "00:30-00:45",
+          "period": "15m",
+          "volume": 40.33 ,
+          "volume_%": "-30%",
+          "price": 0.02690529
+        },
+        {
+          "id": 124,
+          "parentId": 116,
+          "coin": "00:45-00:00",
+          "period": "15m",
+          "volume": 55.33 ,
+          "volume_%": "+20%",
+          "price": 0.02690529
+        },
+        {
+          "id": 125,
+          "parentId": 120,
+          "coin": "00:00-00:15",
+          "period": "5m",
+          "volume": 20.33 ,
+          "volume_%": "+6%",
+          "price": 0.02690529
+        },
+        {
+          "id": 126,
+          "parentId": 120,
+          "coin": "00:00-00:15",
+          "period": "5m",
+          "volume": 25.33 ,
+          "volume_%": "+30%",
+          "price": 0.02690529
+        },
+        {
+          "id": 127,
+          "parentId": 120,
+          "coin": "00:00-00:15",
+          "period": "5m",
+          "volume": 20.33 ,
+          "volume_%": "-20%",
+          "price": 0.02690529
+        },
+        {
+            "id": 202,
+            "parentId": 29,
+            "coin": "06.05-12.05",
+            "period": "1w",
+            "volume": 45355.33 ,
+            "volume_%": "+12%",
+            "price": 0.02690529
+          },
+          {
+            "id": 203,
+            "parentId": 202,
+            "coin": "06.05",
+            "period": "1d",
+            "volume": 6323.33 ,
+            "volume_%": "+7%",
+            "price": 0.02690529
+          },
+          {
+            "id": 204,
+            "parentId": 202,
+            "coin": "07.05",
+            "period": "1d",
+            "volume": 5899.33 ,
+            "volume_%": "+3%",
+            "price": 0.02690529
+          },
+          {
+            "id": 205,
+            "parentId": 202,
+            "coin": "08.05",
+            "period": "1d",
+            "volume": 4642.33 ,
+            "volume_%": "+10%",
+            "price": 0.02690529
+          },
+          {
+            "id": 206,
+            "parentId": 202,
+            "coin": "09.05",
+            "period": "1d",
+            "volume": 5700.33 ,
+            "volume_%": "-5%",
+            "price": 0.02690529
+          },
+        {
+          "id": 207,
+          "parentId": 202,
+          "coin": "10.05",
+          "period": "1d",
+          "volume": 8532.33 ,
+          "volume_%": "-15%",
+          "price": 0.02690529
+        },
+        {
+            "id": 208,
+            "parentId": 202,
+            "coin": "11.05",
+            "period": "1d",
+            "volume": 5321.33 ,
+            "volume_%": "+25%",
+            "price": 0.02690529
+          },
+          {
+            "id": 209,
+            "parentId": 202,
+            "coin": "12.05",
+            "period": "1d",
+            "volume": 3542.33 ,
+            "volume_%": "+13%",
+            "price": 0.02690529
+          },
+          {
+            "id": 210,
+            "parentId": 203,
+            "coin": "00:00-04:00",
+            "period": "4h",
+            "volume": 1580.33 ,
+            "volume_%": "+4%",
+            "price": 0.02690529
+          },
+          {
+            "id": 211,
+            "parentId": 203,
+            "coin": "04:00-08:00",
+            "period": "4h",
+            "volume": 1202.33 ,
+            "volume_%": "+10%",
+            "price": 0.02690529
+          },
+          {
+            "id": 212,
+            "parentId": 203,
+            "coin": "08:00-12:00",
+            "period": "4h",
+            "volume": 1502.33 ,
+            "volume_%": "-13%",
+            "price": 0.02690529
+          },
+          {
+            "id": 213,
+            "parentId": 203,
+            "coin": "12:00-16:00",
+            "period": "4h",
+            "volume": 1802.33 ,
+            "volume_%": "+6%",
+            "price": 0.02690529
+          },
+          {
+            "id": 214,
+            "parentId": 203,
+            "coin": "16:00-20:00",
+            "period": "4h",
+            "volume": 1982.33 ,
+            "volume_%": "+2%",
+            "price": 0.02690529
+          },
+          {
+            "id": 215,
+            "parentId": 203,
+            "coin": "20:00-24:00",
+            "period": "4h",
+            "volume": 1023.33 ,
+            "volume_%": "-21%",
+            "price": 0.02690529
+          },
+          {
+            "id": 216,
+            "parentId": 210,
+            "coin": "00:00-01:00",
+            "period": "1h",
+            "volume": 250.33 ,
+            "volume_%": "+3%",
+            "price": 0.02690529
+          },
+          {
+            "id": 217,
+            "parentId": 210,
+            "coin": "01:00-02:00",
+            "period": "1h",
+            "volume": 300.33 ,
+            "volume_%": "+15%",
+            "price": 0.02690529
+          },
+          {
+            "id": 218,
+            "parentId": 220,
+            "coin": "02:00-03:00",
+            "period": "1h",
+            "volume": 182.33 ,
+            "volume_%": "-50%",
+            "price": 0.02690529
+          },
+          {
+            "id": 219,
+            "parentId": 210,
+            "coin": "03:00-04:00",
+            "period": "1h",
+            "volume": 204.33 ,
+            "volume_%": "+12%",
+            "price": 0.02690529
+          },
+          {
+            "id": 220,
+            "parentId": 216,
+            "coin": "00:00-00:15",
+            "period": "15m",
+            "volume": 55.33 ,
+            "volume_%": "+8%",
+            "price": 0.02690529
+          },
+          {
+            "id": 222,
+            "parentId": 216,
+            "coin": "00:15-00:30",
+            "period": "15m",
+            "volume": 63.33 ,
+            "volume_%": "+10%",
+            "price": 0.02690529
+          },   
+          {
+            "id": 223,
+            "parentId": 216,
+            "coin": "00:30-00:45",
+            "period": "15m",
+            "volume": 40.33 ,
+            "volume_%": "-30%",
+            "price": 0.02690529
+          },
+          {
+            "id": 224,
+            "parentId": 216,
+            "coin": "00:45-00:00",
+            "period": "15m",
+            "volume": 55.33 ,
+            "volume_%": "+20%",
+            "price": 0.02690529
+          },
+          {
+            "id": 225,
+            "parentId": 220,
+            "coin": "00:00-00:15",
+            "period": "5m",
+            "volume": 20.33 ,
+            "volume_%": "+6%",
+            "price": 0.02690529
+          },
+          {
+            "id": 226,
+            "parentId": 220,
+            "coin": "00:00-00:15",
+            "period": "5m",
+            "volume": 25.33 ,
+            "volume_%": "+30%",
+            "price": 0.02690529
+          },
+          {
+            "id": 227,
+            "parentId": 220,
+            "coin": "00:00-00:15",
+            "period": "5m",
+            "volume": 20.33 ,
+            "volume_%": "-20%",
+            "price": 0.02690529
+          },
   ]
