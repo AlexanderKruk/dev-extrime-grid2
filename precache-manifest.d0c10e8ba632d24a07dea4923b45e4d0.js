acb16668494e717fc686a8370a177583ae9f3ef5
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-20 10:30:26 +0300

    Updates

---
+++ precache-manifest.d0c10e8ba632d24a07dea4923b45e4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be207621053ccca99a4d09db66ec777a",
+    "url": "/dev-extrime-grid2/index.html"
+  },
+  {
+    "revision": "b41407221fca1783c82a",
+    "url": "/dev-extrime-grid2/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "b98c524d5a4fe7b7f393",
+    "url": "/dev-extrime-grid2/static/js/2.3ce2e58d.chunk.js"
+  },
+  {
+    "revision": "b41407221fca1783c82a",
+    "url": "/dev-extrime-grid2/static/js/main.a13ea034.chunk.js"
+  },
+  {
+    "revision": "38bdde7f523d5243d8e4",
+    "url": "/dev-extrime-grid2/static/js/runtime~main.c29e74ea.js"
+  }
+]);
